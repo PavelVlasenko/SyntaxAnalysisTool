338dd7bc234ead113f8897309f283e02c41cd503
PV <vlasenko-pav@yandex.ru>
2018-03-12 18:39:31 +0700

    package refactoring

--- src/main/java/tool/visitors/cfg/PythonCfgVisitor.java
+++ src/main/java/tool/visitors/cfg/PythonCfgVisitor.java
@@ -38,10 +38,8 @@ public class PythonCfgVisitor extends Python3BaseListener {
         exitNode.noOtherSuccessor(true);
 
         entryNode.setExitNode(exitNode);
-        entryNode.setLineNumber(1); //TODO
-        entryNode.setFilePath(""); //TODO
         exitNode.setLineNumber(entryNode.getLineNumber());
-        exitNode.setFilePath(""); //TODO
+        exitNode.setFilePath("");
 
         breakables = new LinkedList<>();
         continuables = new LinkedList<>();
@@ -80,17 +78,8 @@ public class PythonCfgVisitor extends Python3BaseListener {
     @Override
     public void enterIf_stmt(Python3Parser.If_stmtContext ctx) {
         IfBeginNode ifBeginNode = new IfBeginNode(getId() + "IfBegin");
-        ConditionNode condNode = new ConditionNode(getId() +"Condition");//TODO
+        ConditionNode condNode = new ConditionNode(getId() +"Condition");
         IfEndNode ifEndNode = new IfEndNode(getId() + "IfEnd");
-
-        ifBeginNode.setLineNumber(1); //TODO
-        ifBeginNode.setFilePath(""); //TODO
-        condNode.setLineNumber(1); //TODO
-        condNode.setFilePath("");
-        ifEndNode.setLineNumber(1);
-        ifEndNode.setFilePath("");
-
-        //condNode.setUsedVars(this.getUsedVar((SimpleNode) node.jjtGetChild(0)));
 
         entryNode.addNodeToLeaves(ifBeginNode);
         ifBeginNode.addSuccessor(condNode);
@@ -99,221 +88,16 @@ public class PythonCfgVisitor extends Python3BaseListener {
         ifends.push(ifEndNode);
 
         Python3Parser.SuiteContext suiteContext = ctx.getChild(Python3Parser.SuiteContext.class, 1);
-//        if(suiteContext != null) {
-//            enterElseStatement(suiteContext);
-//        }
     }
-
-//    public void enterElseStatement(Python3Parser.SuiteContext ctx) {
-//        GraphNode condNode = conditions.getFirst();
-//        GraphNode ifEndNode = ifends.getFirst();
-//
-//        GraphNode trueCaseSubGraph = condNode.getSuccessors().get(0);
-//        condNode.removeSuccessor(0);
-//
-//        if(condNode.getSuccessors().isEmpty()) {
-//            condNode.addSuccessor(ifEndNode);
-//        }
-//
-//        condNode.addSuccessor(0, trueCaseSubGraph);
-//        condNode.addNodeToLeaves(ifEndNode);
-//
-//        conditions.pop();
-//        ifends.pop();
-//    }
 
     @Override
     public void exitIf_stmt(Python3Parser.If_stmtContext ctx) {
 
     }
 
-//    public Object visit(SwitchStatement node, Object data) {
-//        SwitchBeginNode switchBeginNode = new SwitchBeginNode("");
-//        SwitchEndNode switchEndNode = new SwitchEndNode("");
-//        switchEndNode.noOtherSuccessor(true);
-//
-//        switchBeginNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        switchBeginNode.setFilePath(currentFile);
-//        switchEndNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        switchEndNode.setFilePath(currentFile);
-//
-//        entryNode.addNodeToLeaves(switchBeginNode);
-//
-//        continuables.push(switchBeginNode);
-//        breakables.push(switchEndNode);
-//        switchBreaked = false;
-//
-//        propagate(node, data);
-//
-//        continuables.pop();
-//        breakables.pop();
-//        switchBreaked = false;
-//
-//        switchEndNode.noOtherSuccessor(false);
-//
-//        return data;
-//    }
-//
-//    // case
-//    public Object visit(SwitchBlockStatementGroup node, Object data) {
-//        GraphNode caseNode;
-//        if(node.jjtGetFirstToken().toString() == "case")
-//            caseNode = new CaseNode("");
-//        else
-//            caseNode = new DefaultStmtNode("");
-//
-//
-//        caseNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        caseNode.setFilePath(currentFile);
-//
-//        if(switchBreaked)
-//            continuables.getFirst().addSuccessor(caseNode);
-//        else
-//            entryNode.addNodeToLeaves(caseNode);
-//
-//        switchBreaked = false;
-//
-//        propagate(node, data);
-//
-//        return data;
-//    }
-//
-//    public Object visit(WhileStatement node, Object data) {
-//        WhileBeginNode whileBeginNode = new WhileBeginNode("");
-//        ConditionNode condNode = new ConditionNode(this.readLine(node.jjtGetFirstToken().beginLine));
-//        WhileEndNode whileEndNode = new WhileEndNode("");
-//        whileEndNode.noOtherSuccessor(true);
-//
-//        whileBeginNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        whileBeginNode.setFilePath(currentFile);
-//        condNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        condNode.setFilePath(currentFile);
-//        whileEndNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        whileEndNode.setFilePath(currentFile);
-//
-//        condNode.setUsedVars(this.getUsedVar((SimpleNode) node.jjtGetChild(0)));
-//
-//        entryNode.addNodeToLeaves(whileBeginNode);
-//        whileBeginNode.addSuccessor(condNode);
-//
-//        continuables.push(whileBeginNode);
-//        breakables.push(whileEndNode);
-//
-//        propagate(node, condNode);
-//
-//        continuables.pop();
-//        breakables.pop();
-//
-//        condNode.addNodeToLeaves(whileBeginNode);
-//        condNode.addSuccessor(whileEndNode);
-//
-//        whileEndNode.noOtherSuccessor(false);
-//
-//        return whileEndNode;
-//    }
-//
-//    public Object visit(DoStatement node, Object data) {
-//        DoWhileBeginNode doWhileBeginNode = new DoWhileBeginNode("");
-//        ConditionNode condNode = new ConditionNode("");
-//        DoWhileEndNode doWhileEndNode = new DoWhileEndNode("");
-//        doWhileEndNode.noOtherSuccessor(true);
-//
-//        doWhileBeginNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        doWhileBeginNode.setFilePath(currentFile);
-//        condNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        condNode.setFilePath(currentFile);
-//        doWhileEndNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        doWhileEndNode.setFilePath(currentFile);
-//
-//        entryNode.addNodeToLeaves(doWhileBeginNode);
-//
-//        continuables.push(doWhileBeginNode);
-//        breakables.push(doWhileEndNode);
-//
-//        propagate(node, doWhileBeginNode);
-//
-//        continuables.pop();
-//        breakables.pop();
-//
-//        doWhileBeginNode.addNodeToLeaves(condNode);
-//        condNode.addSuccessor(doWhileBeginNode);
-//        condNode.addSuccessor(doWhileEndNode);
-//
-//        doWhileEndNode.noOtherSuccessor(false);
-//
-//        return doWhileEndNode;
-//    }
-//
-//    public Object visit(ForStatement node, Object data) {
-//        ForBeginNode forBeginNode = new ForBeginNode("");
-//        ConditionNode condNode = new ConditionNode("");
-//        ForEndNode forEndNode = new ForEndNode("");
-//        forEndNode.noOtherSuccessor(true);
-//
-//        forBeginNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        forBeginNode.setFilePath(currentFile);
-//        condNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        condNode.setFilePath(currentFile);
-//        forEndNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        forEndNode.setFilePath(currentFile);
-//
-//        entryNode.addNodeToLeaves(forBeginNode);
-//        forBeginNode.addSuccessor(condNode);
-//
-//        continuables.push(forBeginNode);
-//        breakables.push(forEndNode);
-//
-//        propagate(node, condNode);
-//
-//        continuables.pop();
-//        breakables.pop();
-//
-//        condNode.addNodeToLeaves(forBeginNode);
-//        condNode.addSuccessor(forEndNode);
-//
-//        forEndNode.noOtherSuccessor(false);
-//
-//        return forEndNode;
-//    }
-//
-//    public Object visit(BreakStatement node, Object data) {
-//        BreakStmtNode breakNode = new BreakStmtNode("");
-//
-//        breakNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        breakNode.setFilePath(currentFile);
-//
-//        entryNode.addNodeToLeaves(breakNode);
-//        if(!breakables.isEmpty())
-//            breakNode.addSuccessor(breakables.getFirst());
-//        breakNode.noOtherSuccessor(true);
-//
-//        propagate(node, breakNode);
-//
-//        switchBreaked = true;
-//
-//        return data;
-//    }
-//
-//    public Object visit(ContinueStatement node, Object data) {
-//        ContinueStmtNode continueNode = new ContinueStmtNode("");
-//        continueNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//        continueNode.setFilePath(currentFile);
-//        entryNode.addNodeToLeaves(continueNode);
-//        if(!continuables.isEmpty())
-//            continueNode.addSuccessor(continuables.getFirst());
-//        continueNode.noOtherSuccessor(true);
-//
-//        propagate(node, continueNode);
-//
-//        return continueNode;
-//    }
-
-
     @Override
     public void enterReturn_stmt(Python3Parser.Return_stmtContext ctx) {
         ReturnStmtNode returnNode = new ReturnStmtNode(getId() + "Return");
-        returnNode.setLineNumber(1);//TODO
-        returnNode.setFilePath(""); //TODO
         entryNode.addNodeToLeaves(returnNode);
         if(returnNode.getPredecessors().size() > 0) {
             returnNode.addSuccessor(exitNode);
@@ -323,62 +107,17 @@ public class PythonCfgVisitor extends Python3BaseListener {
         switchBreaked = true;
     }
 
-//    public Object visit(VariableDeclarator node, Object data) {
-//        if(node.jjtGetChild(1).jjtGetNumChildren() > 0) {
-//            VarDeclAssignNode varDeclAssignNode = new VarDeclAssignNode(node.jjtGetFirstToken().toString());
-//            varDeclAssignNode.setVarName(node.jjtGetFirstToken().toString());
-//            varDeclAssignNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//            varDeclAssignNode.setFilePath(currentFile);
-//            entryNode.addNodeToLeaves(varDeclAssignNode);
-//
-//            SimpleNode expression = (SimpleNode) node.jjtGetChild(1).jjtGetChild(0).jjtGetChild(0);
-//            varDeclAssignNode.setUsedVars(this.getUsedVar(expression));
-//        } else {
-//            VarDeclNode varDeclNode = new VarDeclNode(node.jjtGetFirstToken().toString());
-//            varDeclNode.setVarName(node.jjtGetFirstToken().toString());
-//            varDeclNode.setLineNumber(node.jjtGetFirstToken().beginLine);
-//            varDeclNode.setFilePath(currentFile);
-//            entryNode.addNodeToLeaves(varDeclNode);
-//        }
-//
-//
-//        propagate(node, data);
-//        return data;
-//    }
-
     @Override
     public void enterExpr_stmt(Python3Parser.Expr_stmtContext ctx) {
         String name = ctx.getText();
-//        if(name.equals("++") || name.equals("--")) {
-//            name = node.jjtGetFirstToken().next.toString();
-//        }
-
-//        String line = readLine(node.jjtGetFirstToken().beginLine);
         String line = ctx.getText();
 
         if(line.matches("\\s*\\w*\\s*\\(.*")) {
-            //MethodCallNode methodCallNode = new MethodCallNode(line);
             MethodCallNode methodCallNode = new MethodCallNode(getId() + "Method call");
-            methodCallNode.setLineNumber(1); //TODO
-            methodCallNode.setFilePath(""); //TODO
-           //HashSet<String> usedVars = this.getUsedVar((SimpleNode) node.jjtGetChild(0));
-           // usedVars.remove(name);
-          //  methodCallNode.setUsedVars(usedVars);
-
             entryNode.addNodeToLeaves(methodCallNode);
         } else {
-            //VarAssignNode varAssignNode = new VarAssignNode(line);
             VarAssignNode varAssignNode = new VarAssignNode(getId() + "Var declaration");
             varAssignNode.setVarName(name);
-            varAssignNode.setLineNumber(1); //TODO
-            varAssignNode.setFilePath(""); //TODO
-
-//            HashSet<String> usedVars = this.getUsedVar((SimpleNode) node.jjtGetChild(0));
-//            if(!line.matches(".*=.*"+name+".*")) {
-//                usedVars.remove(name);
-//            }
-//            varAssignNode.setUsedVars(usedVars);
-
             entryNode.addNodeToLeaves(varAssignNode);
         }
     }
